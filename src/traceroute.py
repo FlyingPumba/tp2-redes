74a283d2146e92021d8b8e1b7e8ead616f08acea
Martin Jedwabny <martiniedva@gmail.com>
2016-05-31 20:03:24 -0300

    WIP - Funcionando basico

--- src/traceroute.py
+++ src/traceroute.py
@@ -3,16 +3,16 @@ from scapy.all import *
 
 if __name__ == "__main__":
     hostname = "google.com"
-    for i in range(1, 28):
-        pkt = IP(dst=hostname, ttl=i) / UDP(dport=33434)
+    for i in range(1, 30):
+        pkt = IP(dst=hostname, ttl=i) / ICMP()
         # Send the packet and get a reply
-        reply = sr1(pkt, verbose=0)
+        reply = sr1(pkt, verbose=0, timeout=2)
         if reply is None:
-            # No reply =(
-            break
-        elif reply.type == 3:
+            print "%d hops away: *" % i
+        elif reply.type == 0:
             # We've reached our destination
-            print "Done!", reply.src
+            print "%d hops away: " % i , reply.src
+            print "Done!"
             break
         else:
             # We're in the middle somewhere
