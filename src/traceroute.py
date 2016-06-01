6fb4cf9fb662abceae198a1b3d229f8d56910994
Martin Jedwabny <martiniedva@gmail.com>
2016-06-01 09:44:14 -0300

    RTTs

--- src/traceroute.py
+++ src/traceroute.py
@@ -5,15 +5,20 @@ if __name__ == "__main__":
     hostname = "google.com"
     for i in range(1, 30):
         pkt = IP(dst=hostname, ttl=i) / ICMP()
-        # Send the packet and get a reply
-        reply = sr1(pkt, verbose=0, timeout=2)
+        # Mandar paquete y obtener reply
+        # Guardar en 'ans' una lista de (send, reply) por cada envio (en este caso solo pkt)
+        ans, unans = sr(pkt, verbose=0, timeout=2)
+        # Obtener paquete enviado y respuesta
+        send, reply = ans[0]
         if reply is None:
             print "%d hops away: *" % i
         elif reply.type == 0:
-            # We've reached our destination
-            print "%d hops away: " % i , reply.src
+            # Llegamos al destino (echo reply)
+            RTT  = (reply.time - send.sent_time) * 1000
+            print "%d hops away: %s in %.4f ms" % (i , reply.src, RTT)
             print "Done!"
             break
         else:
-            # We're in the middle somewhere
-            print "%d hops away: " % i , reply.src
+            # Estamos en un hop intermedio (time-exceeded)
+            RTT  = (reply.time - send.sent_time) * 1000
+            print "%d hops away: %s in %.4f ms" % (i , reply.src, RTT)
