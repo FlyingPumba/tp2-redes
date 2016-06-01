40a311a1b761e6e2b23a35a37fa100d7a199be88
fede.183 <fede.183@hotmail.com>
2016-06-01 14:54:06 -0300

    Detalles

--- src/outliers.py
+++ src/outliers.py
@@ -6,12 +6,12 @@ from scipy import stats
 from scapy.all import *
 
 if __name__ == "__main__":
-	if len(sys.argv) < 2:
+    if len(sys.argv) < 2:
         print ''
         print "Usage: " + sys.argv[0] + " <tipo> <datos>"
         print "\tDonde <tipo> puede ser: \"single\", \"pair\""
-    elif len(sys.argv) > 2:
-    	tipoOutlier = sys.argv[0]
+    elif len(sys.argv) == 2:
+        tipoOutlier = sys.argv[0]
         datos = sys.argv[1]
         cantDatos = len(datos)
 
@@ -20,37 +20,30 @@ if __name__ == "__main__":
         	media, desvio
 
         	media = statistics.mean(data)
-		
-        	desvio = statistics.stdev(data)
+            desvio = statistics.stdev(data)
 
-        	valorAbsolutoMin = abs(min_ - media)
-        	valorAbsolutoMax = abs(max_ - media)
+            valorAbsolutoMin = abs(min_ - media)
+            valorAbsolutoMax = abs(max_ - media)
 
-        	t_a2 = stats.t.ppf(1-(0.05/2.),cantDatos -2)
-   		 	tau = (t_a2 * (cantDatos-1) ) /(math.sqrt(cantDatos) * math.sqrt(cantDatos-2 + t_a2**2))
-    		tS = tau * desvio
+            t_a2 = stats.t.ppf(1-(0.05/2.),cantDatos-2)
+            tau = (t_a2 * (cantDatos-1) ) /(math.sqrt(cantDatos) * math.sqrt(cantDatos-2 + t_a2**2))
+            tS = tau * desvio
 
         	if valorAbsolutoMax > valorAbsolutoMin:
-        		if valorAbsolutoMax <= tS:
-        			#Tengo que quitar el elemento
+        		if valorAbsolutoMax > tS:
+        			#Este es el caso en el que tengo que quitar el elemento
         			index
-        			for i = 1 to cantDatos:
+        			for i in range(1, cantDatos):
         				if datos[i] == _max:
         					index = i
         			datos.pop(index)
                     print "Se quito un outlier"
-   			else: 
-   				if valorAbsolutoMin <= tS:
-   					#Tengo que quitar el elemento
+   			else:
+   				if valorAbsolutoMin > tS:
+   					#Este es el caso en el que tengo que quitar el elemento
    					index
-        			for i = 1 to cantDatos:
+        			for i in range(1, cantDatos):
         				if datos[i] == _min:
         					index = i
         			datos.pop(index)
                     print "Se quito un outlier"
-
-        elif tipoOutlier == "pair":
-
-        else:
-            
-
