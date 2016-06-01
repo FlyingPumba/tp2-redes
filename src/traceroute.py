f9f143f3836e0e7eba5762bad3260059765fc994
Ivan Arcuschin <iarcuschin@gmail.com>
2016-06-01 17:58:54 -0300

    Arreglo bug en traceroute y borro codigo duplicado

--- src/traceroute.py
+++ src/traceroute.py
@@ -8,17 +8,20 @@ if __name__ == "__main__":
         # Mandar paquete y obtener reply
         # Guardar en 'ans' una lista de (send, reply) por cada envio (en este caso solo pkt)
         ans, unans = sr(pkt, verbose=0, timeout=2)
+
+        if not ans:
+            # No llego respuesta
+            print "%d hops away: *" % i
+            continue
+
         # Obtener paquete enviado y respuesta
         send, reply = ans[0]
         if reply is None:
             print "%d hops away: *" % i
-        elif reply.type == 0:
-            # Llegamos al destino (echo reply)
-            RTT  = (reply.time - send.sent_time) * 1000
-            print "%d hops away: %s in %.4f ms" % (i , reply.src, RTT)
-            print "Done!"
-            break
         else:
-            # Estamos en un hop intermedio (time-exceeded)
             RTT  = (reply.time - send.sent_time) * 1000
             print "%d hops away: %s in %.4f ms" % (i , reply.src, RTT)
+            if reply.type == 0:
+                # Llegamos al destino (echo reply)
+                print "Done!"
+                break
