b76b4262ee0494cce93c2e50013cf3144fd28e29
fede.183 <fede.183@hotmail.com>
2016-06-01 12:10:41 -0300

    Agrego Outliers con el de thompson casi terminado

---
+++ src/Outliers.py
@@ -0,0 +1,52 @@
+import math
+import statistics
+from operator import itemgetter
+from scipy import stats
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+        print ''
+        print "Usage: " + sys.argv[0] + " <tipo> <datos>"
+        print "\tDonde <tipo> puede ser: \"single\", \"pair\""
+    elif len(sys.argv) > 2:
+    	tipoOutlier = sys.argv[0]
+        datos = sys.argv[1]
+        cantDatos = len(datos)
+
+        if tipoOutlier == "single":
+        	min_, max_ = MinMax(datos)
+        	media, desvio
+
+        	media = statistics.mean(data)
+		
+        	desvio = statistics.stdev(data)
+
+        	valorAbsolutoMin = abs(min_ - media)
+        	valorAbsolutoMax = abs(max_ - media)
+
+        	t_a2 = stats.t.ppf(1-(0.05/2.),cantDatos -2)
+   		 	tau = (t_a2 * (cantDatos-1) ) /(math.sqrt(cantDatos) * math.sqrt(cantDatos-2 + t_a2**2))
+    		tS = tau * desvio
+
+        	if valorAbsolutoMax > valorAbsolutoMin:
+        		if valorAbsolutoMax <= tS:
+        			#Tengo que quitar el elemento
+        			index
+        			for i = 1 to cantDatos:
+        				if datos[i] == _max:
+        					index = i
+        			datos.pop(index)
+   			else: 
+   				if valorAbsolutoMin <= tS:
+   					#Tengo que quitar el elemento
+   					index
+        			for i = 1 to cantDatos:
+        				if datos[i] == _min:
+        					index = i
+        			datos.pop(index)
+
+        elif tipoOutlier == "pair":
+
+        else:
+            
+
